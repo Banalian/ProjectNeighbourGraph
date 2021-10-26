880b092e225d83f7287e1e5cbb516578acfcff3b
CorentinEsn <75800644+CorentinEsn@users.noreply.github.com>
2021-10-26 11:56:42 +0200

    implemented the Strategy Design Patern for the relative neighbor algorithm

---
+++ src/main/java/main/projectneighbourgraph/relativeLinkStrategy.java
@@ -0,0 +1,12 @@
+package main.projectneighbourgraph;
+
+import java.util.ArrayList;
+
+public class relativeLinkStrategy implements LinkStrategy {
+
+    public void link(ArrayList nodeList, int arg){
+        //TODO : write the relative neighbor algorithm
+    }
+
+}
+
