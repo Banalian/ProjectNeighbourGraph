2f4a3e99fe794af0ede122d206f5ab11a7ce9dd9
CorentinEsn <75800644+CorentinEsn@users.noreply.github.com>
2021-10-26 11:52:40 +0200

    implemented the Strategy Design Patern for the egraph algorithm

---
+++ src/main/java/main/projectneighbourgraph/egraphLinkStrategy.java
@@ -0,0 +1,11 @@
+package main.projectneighbourgraph;
+
+import java.util.ArrayList;
+
+public class egraphLinkStrategy implements LinkStrategy {
+
+    public void link(ArrayList nodeList, int arg){
+        //TODO : write the egraph alogrithm
+    }
+
+}
