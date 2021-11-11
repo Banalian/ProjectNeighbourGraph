659fda85c3d4f2e4db6acf8ecfedc479aab53fe5
Banalian <64379456+Banalian@users.noreply.github.com>
2021-11-11 15:24:48 +0100

    feat(Math) : added a distance matrix function

--- src/main/java/main/projectneighbourgraph/math/MathGraph.java
+++ src/main/java/main/projectneighbourgraph/math/MathGraph.java
@@ -109,4 +109,43 @@ public class MathGraph {
     }
 
 
+    /**
+     * Creates a square matrix telling the distance between two nodes
+     *
+     * @param nodeArrayList the list of Nodes to make the matrix from
+     * @return a square matrix of dimension 2 (dim2 array) and nodeNb size filled with the distances between two nodes A and B
+     */
+    private static double[][] makeDistanceMatrix(ArrayList<Node> nodeArrayList){
+        int nodeNb = nodeArrayList.size();
+        int curNode1 = 0;
+        int curNode2 = 0;
+        double[][] distanceMatrix = new double[nodeNb][nodeNb];
+
+        // we go through the list for the first node
+        for (Node node1: nodeArrayList) {
+            curNode2 = 0;
+
+            //for each node to make a duo node1 and node2
+            for(Node node2 : nodeArrayList){
+
+                //if it's two different nodes
+                if((node1!=node2)){
+                    //get the length between them and add it to the matrix
+                    double length = lengthBetweenTwoPoints(node1,node2);
+                    distanceMatrix[curNode1][curNode2] = length;
+                    distanceMatrix[curNode2][curNode1] = length;
+                }else{
+                    //distance is 0 since it's itself
+                    distanceMatrix[curNode1][curNode2] = 0;
+                }
+                curNode2++;
+            }
+
+            curNode1++;
+        }
+
+        return distanceMatrix;
+    }
+
+
 }
